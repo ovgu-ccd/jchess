304be9f21bb197d3575679704483b6c53fd61929
ParticlePeter <ParticlePeter@gmx.de>
2015-01-09 10:03:42 +0100

    event stub methods

--- jchess/Board.java
+++ jchess/Board.java
@@ -98,5 +98,16 @@ public class Board {
         return false;
     }
 
+    // get event methods
+    @Handler public void handleSelectEvent( Coordinates coordinates ) {
+
+    }
+
+
+    // set event methods
+    //void emitt
+
+
+
 }
 
