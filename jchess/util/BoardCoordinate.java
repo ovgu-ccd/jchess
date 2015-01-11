83c751d539584e1dda7268012e44edebe2d8e454
ParticlePeter <ParticlePeter@gmx.de>
2015-01-11 18:34:31 +0100

    Implemented boardToPixelCoordiante

--- jchess/util/BoardCoordinate.java
+++ jchess/util/BoardCoordinate.java
@@ -4,12 +4,11 @@ package jchess.util;
  * Pseudo-Polar Board Coordinates
  */
 public class BoardCoordinate {
-    int a, b, c, i;
+    int a, b, i;
 
     public BoardCoordinate(int a, int b, int i) {
         this.a = a;
         this.b = b;
-        this.c = b - a;
         this.i = i;
     }
 
