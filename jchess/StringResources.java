7e288f2425c4323a0d5256bf8eb19e96396bf977
Robert Heumueller <code@heum.de>
2015-01-20 15:03:59 +0100

    Fixed encoding bug.

--- jchess/StringResources.java
+++ jchess/StringResources.java
@@ -1,5 +1,6 @@
 package jchess;
 
+import java.io.UnsupportedEncodingException;
 import java.util.Locale;
 import java.util.ResourceBundle;
 
@@ -7,8 +8,8 @@ import java.util.ResourceBundle;
  * Created by robert on 05.12.14.
  */
 public enum StringResources {
-    GUI ("jchess.resources.strings.gui"),
-    MAIN ("jchess.resources.strings.main");
+    GUI("jchess.resources.strings.gui"),
+    MAIN("jchess.resources.strings.main");
 
     private ResourceBundle bundle;
     private String baseName;
@@ -19,10 +20,25 @@ public enum StringResources {
     }
 
     public String getString(String key) {
-        return this.bundle.getString(key);
+        String val = this.bundle.getString(key);
+        String encoded = "Encoding Error.";
+        try {
+            return new String(val.getBytes("ISO-8859-1"), "UTF-8");
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+        return encoded;
     }
 
+
     public String getString(String key, Locale locale) {
-        return ResourceBundle.getBundle(baseName, locale).getString(key);
+        String val = ResourceBundle.getBundle(baseName, locale).getString(key);
+        String encoded = "Encoding Error.";
+        try {
+            return new String(val.getBytes("ISO-8859-1"), "UTF-8");
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+        return encoded;
     }
 }
