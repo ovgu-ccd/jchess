1743b520ffd717c2e07826d10dcbae7a8e42d0df
ParticlePeter <ParticlePeter@gmx.de>
2015-01-23 10:42:33 +0100

    Removed non required neighborhood logic from Tile

--- jchess/Tile.java
+++ jchess/Tile.java
@@ -10,55 +10,25 @@ import java.util.Set;
  */
 public class Tile {
 
-    private Tile neighbors[] ;
     private Piece piece;
     private Set<Integer> promotesPlayers;
 
     public Tile()  {
-        neighbors = null;
         piece = null;
         promotesPlayers = new HashSet<>();
-    }
-
-    public void setNeigbors( Tile neighbors[] ) {
-        this.neighbors = neighbors;
-    }
-
-    public Tile getNorth() {
-        return neighbors[0];
-    }
-
-    public Tile getNorthEast() {
-        return neighbors[1];
-    }
-
-    public Tile getSouthEast() {
-        return neighbors[2];
-    }
-
-    public Tile getSouth() {
-        return neighbors[3];
-    }
-
-    public Tile getSouthWest() {
-        return neighbors[4];
-    }
-
-    public Tile getNorthWest() {
-        return neighbors[5];
     }
 
     public Piece getPiece() {
         return this.piece;
     }
 
-    Piece placePiece( Piece newPiece ) {
+    public Piece placePiece( Piece newPiece ) {
         Piece oldPiece = this.piece;
         this.piece = newPiece;
         return oldPiece;
     }
 
-    Piece removePiece() {
+    public Piece removePiece() {
         Piece oldPiece = this.piece;
         this.piece = null;
         return oldPiece;
