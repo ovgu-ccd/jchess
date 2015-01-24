01139070ab7d6768d8f2e1392b4bfd583e167179
travis-ci <info@apfohl.com>
2015-01-24 17:20:08 +0000

    Automated documentation build for changeset accad28a616cb924be6ee9b423d61b9153a2ffe9.

--- search/classes_69.js
+++ search/classes_69.js
@@ -1,5 +1,4 @@
 var searchData=
 [
-  ['iosystem',['IOSystem',['../interfacejchess_1_1game_1_1IOSystem.html',1,'jchess::game']]],
-  ['iosystemmock',['IOSystemMock',['../classjchess_1_1tests_1_1game_1_1IOSystemMock.html',1,'jchess::tests::game']]]
+  ['iosystem',['IOSystem',['../interfacejchess_1_1game_1_1IOSystem.html',1,'jchess::game']]]
 ];
---
+++ search/classes_6d.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['movetest',['MoveTest',['../classjchess_1_1tests_1_1game_1_1MoveTest.html',1,'jchess::tests::game']]]
+];
