3992d48764413a56ecd74870555abb6d9547aab4
ParticlePeter <ParticlePeter@gmx.de>
2015-01-25 14:17:27 +0100

    Additional Documentation for Tile methods

--- jchess/game/board/Tile.java
+++ jchess/game/board/Tile.java
@@ -18,26 +18,49 @@ public class Tile {
         promotesPlayers = new HashSet<>();
     }
 
+    /**
+     * Access a Piece on a Tile. Does not alter the state of that Tile.
+     * Used to query if a Piece is on this Tile
+     */
     public Piece getPiece() {
         return this.piece;
     }
 
+    /**
+     * Places a Piece onto this Tile
+     * Returns the Piece which was on this Tile before
+     * or null if no Piece was there
+     *
+     * @param newPiece the Piece which is to be placed
+     */
     public Piece placePiece(Piece newPiece) {
         Piece oldPiece = this.piece;
         this.piece = newPiece;
         return oldPiece;
     }
 
+    /**
+     * Removes a Piece from this Tile.
+     * Returns either the old Piece or null
+     */
     public Piece removePiece() {
         Piece oldPiece = this.piece;
         this.piece = null;
         return oldPiece;
     }
 
+    /**
+     * Registers this Field as promoting Tile ( in case a Pawn( playerID ) reaches this Tile )
+     *
+     * @param playerID
+     */
     public void addPromotionForPlayer(int playerID) {
         promotesPlayers.add(playerID);
     }
 
+    /**
+     * Registers this Field as promoting Tile ( in case a Pawn( playerID ) reaches this Tile )
+     */
     public boolean isPromotionTileFor(int playerID) {
         return promotesPlayers.contains(playerID);
     }
