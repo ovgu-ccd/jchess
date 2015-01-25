f5a6539525e176507e09b46e54f90351783e930b
ParticlePeter <ParticlePeter@gmx.de>
2015-01-25 14:48:09 +0100

    Added some Access tests. Board needs an Exception for invalid Boardcoordinates

--- jchess/tests/BoardTest.java
+++ jchess/tests/BoardTest.java
@@ -26,6 +26,20 @@ public class BoardTest {
     }*/
 
     @Test
+    public void testGetTile() {
+        assert( board.getTile(  0,  0 ) == board.getTile(  0));
+        assert( board.getTile(  0,  7 ) == board.getTile(  7));
+        assert( board.getTile(  1,  0 ) == board.getTile(  8));
+        assert( board.getTile(  4,  4 ) == board.getTile( 42));
+        assert( board.getTile(  8,  5 ) == board.getTile( 96));
+        assert( board.getTile( 11, 12 ) == board.getTile(139));
+        assert( board.getTile( 11, 13 ) == board.getTile(140));
+        assert( board.getTile( 12, 11 ) == board.getTile(148));
+        assert( board.getTile( 13,  6 ) == board.getTile(152));
+        assert( board.getTile( 14, 14 ) == board.getTile(168));
+    }
+
+    @Test
     public void testUndo() {
         boolean undo = board.undo();
         assertTrue(undo);
