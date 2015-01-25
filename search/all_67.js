ca8447ac561a31f8134910e85692a576d04cd8c9
travis-ci <info@apfohl.com>
2015-01-25 13:19:39 +0000

    Automated documentation build for changeset 3992d48764413a56ecd74870555abb6d9547aab4.

--- search/all_67.js
+++ search/all_67.js
@@ -6,6 +6,7 @@ var searchData=
   ['genericerrorevent',['GenericErrorEvent',['../classjchess_1_1eventbus_1_1events_1_1GenericErrorEvent.html',1,'jchess::eventbus::events']]],
   ['genericerroreventhandler',['GenericErrorEventHandler',['../classjchess_1_1tests_1_1eventbus_1_1events_1_1GenericErrorEventTest_1_1GenericErrorEventHandler.html',1,'jchess::tests::eventbus::events::GenericErrorEventTest']]],
   ['genericerroreventtest',['GenericErrorEventTest',['../classjchess_1_1tests_1_1eventbus_1_1events_1_1GenericErrorEventTest.html',1,'jchess::tests::eventbus::events']]],
+  ['getpiece',['getPiece',['../classjchess_1_1game_1_1board_1_1Tile.html#a533dd73628c5cd74965055b948332b6e',1,'jchess::game::board::Tile']]],
   ['gettile',['getTile',['../classjchess_1_1game_1_1board_1_1Board.html#ada5b8ae859aa872df233f6a482fccfa9',1,'jchess::game::board::Board.getTile(int tileIndex)'],['../classjchess_1_1game_1_1board_1_1Board.html#a14c1e908ba2a386d65898b1d0a439c0a',1,'jchess::game::board::Board.getTile(int a, int b)'],['../classjchess_1_1game_1_1board_1_1Board.html#a361b38b41f8b593dccb13dcbcee20708',1,'jchess::game::board::Board.getTile(BoardCoordinate boardCoordinate)']]],
   ['gui',['GUI',['../classjchess_1_1gui_1_1GUI.html',1,'jchess::gui']]],
   ['guiconnector',['GUIConnector',['../classjchess_1_1gui_1_1GUIConnector.html',1,'jchess::gui']]]
--- search/functions_69.js
+++ search/functions_69.js
@@ -1,5 +1,5 @@
 var searchData=
 [
   ['initfigures',['initFigures',['../classjchess_1_1game_1_1board_1_1Board.html#a994f277e38ff66fd38f1675d32708b2a',1,'jchess::game::board::Board']]],
-  ['initneighborhood',['initNeighborhood',['../classjchess_1_1game_1_1board_1_1Board.html#a50f333f045b70224c0ee12f345f045d0',1,'jchess::game::board::Board']]]
+  ['ispromotiontilefor',['isPromotionTileFor',['../classjchess_1_1game_1_1board_1_1Tile.html#a9d5a65481d20136304bca2e1cd24839b',1,'jchess::game::board::Tile']]]
 ];
