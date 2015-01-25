b65bed01489b1f5e715ca08e5d0babb571094ea8
travis-ci <info@apfohl.com>
2015-01-25 18:08:55 +0000

    Automated documentation build for changeset b174ecf34ec46743e8a81953c3e29d79ba3e2b3c.

--- search/all_62.js
+++ search/all_62.js
@@ -1,6 +1,7 @@
 var searchData=
 [
   ['bishop',['Bishop',['../classjchess_1_1game_1_1pieces_1_1Bishop.html',1,'jchess::game::pieces']]],
+  ['bishopmovementtest',['BishopMovementTest',['../classtests_1_1game_1_1BishopMovementTest.html',1,'tests::game']]],
   ['board',['Board',['../classjchess_1_1game_1_1board_1_1Board.html',1,'jchess::game::board']]],
   ['boardcoordinate',['BoardCoordinate',['../classjchess_1_1util_1_1BoardCoordinate.html',1,'jchess::util']]],
   ['boardcoordinatetoindex',['boardCoordinateToIndex',['../classjchess_1_1util_1_1CoordinateConverter.html#a79cbd1633bbbf4980437c3bc9491cd9b',1,'jchess::util::CoordinateConverter']]],
