e5481a0a014af5cbc207fbaf9f3d46ed9fe6091e
travis-ci <info@apfohl.com>
2015-01-25 15:35:42 +0000

    Automated documentation build for changeset 1d54d7ca800cae349a14744dbc1a5b8e1e1ec97b.

--- search/all_74.js
+++ search/all_74.js
@@ -1,9 +1,12 @@
 var searchData=
 [
+  ['events',['events',['../namespacetests_1_1eventbus_1_1events.html',1,'tests::eventbus']]],
+  ['game',['game',['../namespacetests_1_1game.html',1,'tests']]],
   ['tabbedpaneicon',['TabbedPaneIcon',['../classjchess_1_1gui_1_1TabbedPaneIcon.html',1,'jchess::gui']]],
   ['testconfiguration',['TestConfiguration',['../classjchess_1_1configurations_1_1TestConfiguration.html',1,'jchess::configurations']]],
+  ['tests',['tests',['../namespacetests.html',1,'']]],
   ['tile',['Tile',['../classjchess_1_1game_1_1board_1_1Tile.html',1,'jchess::game::board']]],
   ['tilefilter',['TileFilter',['../classjchess_1_1game_1_1board_1_1TileFilter.html',1,'jchess::game::board']]],
-  ['trimstring',['trimString',['../classjchess_1_1gui_1_1DrawLocalSettings.html#a6d2bb6d8560487d36593c65f76cf22b7',1,'jchess::gui::DrawLocalSettings']]],
+  ['trimstring',['trimString',['../classjchess_1_1gui_1_1DrawLocalSettings.html#a6e85617df168e695f19290b12570535d',1,'jchess::gui::DrawLocalSettings']]],
   ['types',['Types',['../enumjchess_1_1eventbus_1_1events_1_1UpdateStatusMessageEvent_1_1Types.html',1,'jchess::eventbus::events::UpdateStatusMessageEvent']]]
 ];
--- search/classes_73.js
+++ search/classes_73.js
@@ -1,9 +1,9 @@
 var searchData=
 [
   ['selectevent',['SelectEvent',['../classjchess_1_1eventbus_1_1events_1_1SelectEvent.html',1,'jchess::eventbus::events']]],
-  ['selecteventhandler',['SelectEventHandler',['../classjchess_1_1tests_1_1eventbus_1_1events_1_1SelectEventTest_1_1SelectEventHandler.html',1,'jchess::tests::eventbus::events::SelectEventTest']]],
-  ['selecteventtest',['SelectEventTest',['../classjchess_1_1tests_1_1eventbus_1_1events_1_1SelectEventTest.html',1,'jchess::tests::eventbus::events']]],
+  ['selecteventhandler',['SelectEventHandler',['../classtests_1_1eventbus_1_1events_1_1SelectEventTest_1_1SelectEventHandler.html',1,'tests::eventbus::events::SelectEventTest']]],
+  ['selecteventtest',['SelectEventTest',['../classtests_1_1eventbus_1_1events_1_1SelectEventTest.html',1,'tests::eventbus::events']]],
   ['statusanimator',['StatusAnimator',['../classjchess_1_1gui_1_1StatusAnimator.html',1,'jchess::gui']]],
   ['stringresources',['StringResources',['../enumjchess_1_1util_1_1StringResources.html',1,'jchess::util']]],
-  ['stringresourcestest',['StringResourcesTest',['../classjchess_1_1tests_1_1StringResourcesTest.html',1,'jchess::tests']]]
+  ['stringresourcestest',['StringResourcesTest',['../classtests_1_1StringResourcesTest.html',1,'tests']]]
 ];
