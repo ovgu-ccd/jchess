e9352005e13b01ffd5ee1033a5976ec05ef6235a
travis-ci <info@apfohl.com>
2015-01-25 18:24:19 +0000

    Automated documentation build for changeset 9efb374a0b7c22bff319dd802376078f447640ef.

--- search/classes_71.js
+++ search/classes_71.js
@@ -1,4 +1,5 @@
 var searchData=
 [
-  ['queen',['Queen',['../classjchess_1_1game_1_1pieces_1_1Queen.html',1,'jchess::game::pieces']]]
+  ['queen',['Queen',['../classjchess_1_1game_1_1pieces_1_1Queen.html',1,'jchess::game::pieces']]],
+  ['queenmovementtest',['QueenMovementTest',['../classtests_1_1game_1_1QueenMovementTest.html',1,'tests::game']]]
 ];
