8db3b591c84c0fadf52be01a47b0234819a6dbe0
travis-ci <info@apfohl.com>
2015-01-25 16:10:06 +0000

    Automated documentation build for changeset d830984a582eab555488d4fe705cd093eea08104.

--- search/functions_67.js
+++ search/functions_67.js
@@ -1,4 +1,5 @@
 var searchData=
 [
+  ['getpiece',['getPiece',['../classjchess_1_1game_1_1board_1_1Tile.html#a533dd73628c5cd74965055b948332b6e',1,'jchess::game::board::Tile']]],
   ['gettile',['getTile',['../classjchess_1_1game_1_1board_1_1Board.html#ada5b8ae859aa872df233f6a482fccfa9',1,'jchess::game::board::Board.getTile(int tileIndex)'],['../classjchess_1_1game_1_1board_1_1Board.html#a14c1e908ba2a386d65898b1d0a439c0a',1,'jchess::game::board::Board.getTile(int a, int b)'],['../classjchess_1_1game_1_1board_1_1Board.html#a361b38b41f8b593dccb13dcbcee20708',1,'jchess::game::board::Board.getTile(BoardCoordinate boardCoordinate)']]]
 ];
