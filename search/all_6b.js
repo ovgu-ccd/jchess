5a3a6b5bb8a834d5c2b63bc4c3a6da1c47f57f96
travis-ci <info@apfohl.com>
2015-01-25 17:54:55 +0000

    Automated documentation build for changeset 9e8f90a486f6dfc44e123294abfbb8b70f6cf687.

--- search/all_6b.js
+++ search/all_6b.js
@@ -1,5 +1,6 @@
 var searchData=
 [
   ['king',['King',['../classjchess_1_1game_1_1pieces_1_1King.html',1,'jchess::game::pieces']]],
+  ['kingmovementtest',['KingMovementTest',['../classtests_1_1game_1_1KingMovementTest.html',1,'tests::game']]],
   ['knight',['Knight',['../classjchess_1_1game_1_1pieces_1_1Knight.html',1,'jchess::game::pieces']]]
 ];
