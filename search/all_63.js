21de2c5f0f4c122bff15f0accb371b5b4208b584
travis-ci <info@apfohl.com>
2015-01-25 17:16:43 +0000

    Automated documentation build for changeset c00408611ccd24774b6257cf92f3cf5ff193d5e3.

--- search/all_63.js
+++ search/all_63.js
@@ -2,5 +2,6 @@ var searchData=
 [
   ['controller',['Controller',['../classjchess_1_1eventbus_1_1Controller.html',1,'jchess::eventbus']]],
   ['coordinateconverter',['CoordinateConverter',['../classjchess_1_1util_1_1CoordinateConverter.html',1,'jchess::util']]],
-  ['coordinateconvertertest',['CoordinateConverterTest',['../classtests_1_1util_1_1CoordinateConverterTest.html',1,'tests::util']]]
+  ['coordinateconvertertest',['CoordinateConverterTest',['../classtests_1_1util_1_1CoordinateConverterTest.html',1,'tests::util']]],
+  ['coordinateconvertertest',['CoordinateConverterTest',['../classtests_1_1CoordinateConverterTest.html',1,'tests']]]
 ];
