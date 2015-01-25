8aeb340951a8a55bba31fb8707666752d8d5e5b5
travis-ci <info@apfohl.com>
2015-01-25 19:56:47 +0000

    Automated documentation build for changeset 44d4c8d5563af24cab06c81e097743f9681baf77.

--- search/classes_6c.js
+++ search/classes_6c.js
@@ -1,4 +1,5 @@
 var searchData=
 [
-  ['logging',['Logging',['../enumjchess_1_1util_1_1Logging.html',1,'jchess::util']]]
+  ['logging',['Logging',['../enumjchess_1_1util_1_1Logging.html',1,'jchess::util']]],
+  ['looseconditiontest',['LooseConditionTest',['../classtests_1_1game_1_1LooseConditionTest.html',1,'tests::game']]]
 ];
