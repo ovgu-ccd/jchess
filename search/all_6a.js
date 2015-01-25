b88b5740e4a8c41c4cc3aaa3afc77d2d7ed9841a
travis-ci <info@apfohl.com>
2015-01-25 20:05:26 +0000

    Automated documentation build for changeset be5af6fcf0ee570d2476c62c654ee5bb61c6cdf2.

--- search/all_6a.js
+++ search/all_6a.js
@@ -4,6 +4,5 @@ var searchData=
   ['eventbus',['eventbus',['../namespacejchess_1_1eventbus.html',1,'jchess']]],
   ['game',['game',['../namespacejchess_1_1game.html',1,'jchess']]],
   ['gui',['gui',['../namespacejchess_1_1gui.html',1,'jchess']]],
-  ['jchesstabbedpane',['JChessTabbedPane',['../classjchess_1_1gui_1_1JChessTabbedPane.html',1,'jchess::gui']]],
   ['pieces',['pieces',['../namespacejchess_1_1game_1_1pieces.html',1,'jchess::game']]]
 ];
--- search/classes_6a.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['jchesstabbedpane',['JChessTabbedPane',['../classjchess_1_1gui_1_1JChessTabbedPane.html',1,'jchess::gui']]]
-];
