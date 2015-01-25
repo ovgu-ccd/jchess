1916aac9d02ff6e222ab4b8a4355478bd45de8ec
Robert Heumueller <code@heum.de>
2015-01-25 18:32:37 +0100

    Testing rook movement

--- tests/game/RookMovementTest.java
+++ tests/game/RookMovementTest.java
@@ -8,15 +8,16 @@ import jchess.eventbus.events.SelectEvent;
 import jchess.game.Game;
 import jchess.game.IOSystem;
 import jchess.game.board.Board;
+import jchess.game.board.InvalidBoardCoordinateException;
 import jchess.game.pieces.Pawn;
-import jchess.game.pieces.Piece;
 import jchess.game.pieces.Rook;
 import jchess.util.BoardCoordinate;
 import jchess.util.Logging;
 import org.junit.Before;
 import org.junit.Test;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 /**
  * Created by robert on 24/01/15.
@@ -25,10 +26,13 @@ import static org.junit.Assert.*;
 public class RookMovementTest {
 
     @Singleton
-    static class RookMovementBoard extends Board {
+    public static class RookMovementBoard extends Board {
         @Override
-        protected void initFigures() {
+        protected void initFigures() throws InvalidBoardCoordinateException {
             getTile(0, 0).placePiece(new Rook(0));
+            getTile(6, 0).placePiece(new Pawn(0));
+            getTile(0, 6).placePiece(new Pawn(0));
+            getTile(13, 13).placePiece(new Pawn(1));
         }
     }
 
@@ -57,70 +61,20 @@ public class RookMovementTest {
     }
 
     @Test
-    public void possibleMovesPawn1() {
-        SelectEvent selectEvent = new SelectEvent(game, new BoardCoordinate(6, 0));
+    public void possibleMovesRook() {
+        SelectEvent selectEvent = new SelectEvent(game, new BoardCoordinate(0, 0));
         emitAndSimulateRelay(selectEvent);
 
-        assertEquals(game.getPossibleMovesCoordinates().size(), 3);
-        assertTrue(game.getPossibleMovesCoordinates().contains(new BoardCoordinate(7, 0)));
-        assertTrue(game.getPossibleMovesCoordinates().contains(new BoardCoordinate(7, 1)));
-        assertTrue(game.getPossibleMovesCoordinates().contains(new BoardCoordinate(8, 2)));
+        assertEquals(game.getPossibleMovesCoordinates().size(), 23);
+        for (int i = 1; i < 14; i++) {
+            assertTrue(game.getPossibleMovesCoordinates().contains(new BoardCoordinate(i, i)));
+        }
+        for (int i = 1; i < 6; i++) {
+            assertTrue(game.getPossibleMovesCoordinates().contains(new BoardCoordinate(i, 0)));
+            assertTrue(game.getPossibleMovesCoordinates().contains(new BoardCoordinate(0, i)));
+        }
     }
 
-    @Test
-    public void possibleMovesPawn2() {
-        SelectEvent selectEvent = new SelectEvent(game, new BoardCoordinate(7, 7));
-        emitAndSimulateRelay(selectEvent);
-
-        assertEquals(game.getPossibleMovesCoordinates().size(), 7);
-        assertTrue(game.getPossibleMovesCoordinates().contains(new BoardCoordinate(8, 7)));
-        assertTrue(game.getPossibleMovesCoordinates().contains(new BoardCoordinate(9, 7)));
-        assertTrue(game.getPossibleMovesCoordinates().contains(new BoardCoordinate(8, 8)));
-        assertTrue(game.getPossibleMovesCoordinates().contains(new BoardCoordinate(9, 9)));
-
-        assertTrue(game.getPossibleMovesCoordinates().contains(new BoardCoordinate(9, 8)));
-        assertTrue(game.getPossibleMovesCoordinates().contains(new BoardCoordinate(8, 6)));
-        assertTrue(game.getPossibleMovesCoordinates().contains(new BoardCoordinate(8, 9)));
-    }
-
-    @Test
-    public void movePawn2() {
-        SelectEvent selectEvent = new SelectEvent(game, new BoardCoordinate(7, 7));
-        emitAndSimulateRelay(selectEvent);
-
-        Piece selectedPiece = game.getBoard().getTile(7, 7).getPiece();
-
-        selectEvent = new SelectEvent(game, new BoardCoordinate(8, 7));
-        emitAndSimulateRelay(selectEvent);
-
-
-        assertNull(game.getBoard().getTile(7, 7).getPiece());
-        assertEquals(game.getBoard().getTile(8, 7).getPiece(), selectedPiece);
-    }
-
-    @Test
-    public void strikePawn2() {
-        SelectEvent selectEvent = new SelectEvent(game, new BoardCoordinate(7, 7));
-        emitAndSimulateRelay(selectEvent);
-
-        Piece selectedPiece = game.getBoard().getTile(7, 7).getPiece();
-
-        selectEvent = new SelectEvent(game, new BoardCoordinate(9, 8));
-        emitAndSimulateRelay(selectEvent);
-
-
-        assertNull(game.getBoard().getTile(7, 7).getPiece());
-        assertEquals(game.getBoard().getTile(9, 8).getPiece(), selectedPiece);
-    }
-
-
-    @Test
-    public void possibleMovesPawn3() {
-        SelectEvent selectEvent = new SelectEvent(game, new BoardCoordinate(14, 7));
-        emitAndSimulateRelay(selectEvent);
-
-        assertEquals(game.getPossibleMovesCoordinates().size(), 0);
-    }
 
     void emitAndSimulateRelay(SelectEvent selectEvent) {
         // Simulate relay
