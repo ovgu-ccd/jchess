6f74c4a0cf6764b9111270778c1abafb6f706e66
travis-ci <info@apfohl.com>
2015-01-25 17:36:24 +0000

    Automated documentation build for changeset 1916aac9d02ff6e222ab4b8a4355478bd45de8ec.

--- search/all_72.js
+++ search/all_72.js
@@ -1,5 +1,6 @@
 var searchData=
 [
   ['removepiece',['removePiece',['../classjchess_1_1game_1_1board_1_1Tile.html#a972ebee7e03599b714e26c3c3d734721',1,'jchess::game::board::Tile']]],
-  ['rook',['Rook',['../classjchess_1_1game_1_1pieces_1_1Rook.html',1,'jchess::game::pieces']]]
+  ['rook',['Rook',['../classjchess_1_1game_1_1pieces_1_1Rook.html',1,'jchess::game::pieces']]],
+  ['rookmovementtest',['RookMovementTest',['../classtests_1_1game_1_1RookMovementTest.html',1,'tests::game']]]
 ];
