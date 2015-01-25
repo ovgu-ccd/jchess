e82155febfde5a26059d4506aab9dec2e7b443ce
travis-ci <info@apfohl.com>
2015-01-25 16:40:30 +0000

    Automated documentation build for changeset d105433fa37a976deb84310019c8e89a3f2380f3.

--- search/all_72.js
+++ search/all_72.js
@@ -1,5 +1,4 @@
 var searchData=
 [
-  ['rook',['Rook',['../classjchess_1_1game_1_1pieces_1_1Rook.html',1,'jchess::game::pieces']]],
-  ['rookmovementtest',['RookMovementTest',['../classtests_1_1game_1_1RookMovementTest.html',1,'tests::game']]]
+  ['rook',['Rook',['../classjchess_1_1game_1_1pieces_1_1Rook.html',1,'jchess::game::pieces']]]
 ];
